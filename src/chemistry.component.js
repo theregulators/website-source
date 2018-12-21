131d7a2a2370a5e89a78a97b827785bade4badfd
Jonathan Lam <jlam55555@gmail.com>
2018-12-20 23:44:04 -0500

    updated website with video

--- src/chemistry.component.js
+++ src/chemistry.component.js
@@ -7,7 +7,9 @@ class ChemistryComponent extends React.Component {
     return (<div>
       <h3>Chemistry</h3>
       <AboutDropdownComponent />
-      <p>Specific details about the chemicals and experimental procedures will be added here.</p>
+      <p>The idea of a colorimetric test strip was tested by G-Cubed solutions (Chan et al, 2017) the previous year. The test strip currently consists of a mixture of chemical indicators, a filter mesh and a small strip of plain white printer paper. The printer paper was chosen because it is the cheapest, uniform paper that was found. The chemical mix is made up of TMB or 3,3',5,5'-Tetramethylbenzidine, Horseradish peroxidase, and glucose oxidase dissolved in a mixture of ethanol and deionized water. When it comes into contact with glucose, the glucose oxidase will break it down and produce gluconic acid and hydrogen peroxide. Horseradish peroxidase then oxidizes the TMB using hydrogen peroxide and produces a blue color change. The filter mesh is used to filter out the Red blood cells from the sample of blood. This is done to remove the red color in the blood as to not affect the color of the test. The filter mesh is made up of electrospun polymers with a weave small enough to limit the passage of cells including RBC through it.</p>
+      <p>The mixture of chemicals and the amount to put on the test strip was found based on the effective units of each reagent. The Glucose oxidase mixture was 30 units per ml, the Horseradish Peroxidase was 60 units per ml and it was decided to make the TMB mass equal to the peroxidase due to a lack of unit measurement.</p>
+      <p>The test strip is made by placing the sample blood through the filter mesh, onto the piece of paper then adding the chemical mixture. This was found to be the best order due to the red color that developed from putting the chemical mixture first.</p>
     </div>);
   }
 }
--- src/index.component.js
+++ src/index.component.js
@@ -7,14 +7,20 @@ class IndexComponent extends React.Component {
   render() {
     return (<Row className='mx-0'>
       <Col xs='12' className='p-0'>
-        <Jumbotron fluid className='jumbotron-impression text-center d-flex flex-column justify-content-center text-light mb-0' id='header-jumbotron'>
-          <video autoPlay muted loop id='header-video'>
-            <source src='/assets/header.webm' type='video/webm' />
-          </video>
+        <Jumbotron fluid className='jumbotron-impression text-center d-flex flex-column justify-content-center mb-0 bg-dark text-light' id=''>
           <Container>
-            <img className='mb-4' id='strip-img' src='./assets/strip.svg' alt='The Regulator is a low-cost colorimetric test strip for blood glucose testing.' />
-            <h1 className='display-5'>Low-cost blood sugar monitoring</h1>
-            <h5 className='font-weight-light'><Link to='/about/solution'>How do we do it?</Link></h5>
+            <Row>
+              <Col>
+                <img className='mb-4' id='strip-img' src='./assets/strip.svg' alt='The Regulator is a low-cost colorimetric test strip for blood glucose testing.' />
+                <h1 className='display-5'>Low-cost blood sugar monitoring</h1>
+                <h5 className='font-weight-light'><Link to='/about/solution'>How do we do it?</Link></h5>
+              </Col>
+              <Col>
+                <video muted loop controls id='main-video' className='bg-light'>
+                  <source src='/assets/intro-video.webm' type='video/webm' />
+                </video>
+              </Col>
+            </Row>
           </Container>
         </Jumbotron>
       </Col>
@@ -33,6 +39,17 @@ class IndexComponent extends React.Component {
           <Container>
             <h1 className='display-5'>See what we do</h1>
             <h5 className='font-weight-light'><Link to='/media'>See the gallery.</Link></h5>
+          </Container>
+        </Jumbotron>
+      </Col>
+      <Col xs='12' className='p-0'>
+        <Jumbotron fluid className='jumbotron-impression text-center d-flex flex-column justify-content-center text-light mb-0' id='header-jumbotron'>
+          <video autoPlay muted loop id='header-video'>
+            <source src='/assets/header.webm' type='video/webm' />
+          </video>
+          <Container>
+            <h1 className='display-5'>We are The Regulators</h1>
+            <h5 className='font-weight-light'><Link to='/team'>Meet the team</Link></h5>
           </Container>
         </Jumbotron>
       </Col>
